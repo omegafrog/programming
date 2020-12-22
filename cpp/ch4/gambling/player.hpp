a0adca2c2a0a6c6b3409605c31523a05dec5bc4a
omegafrog <jiwoo22110103@gmail.com>
2020-12-22 11:18:08 +0000

    gambling_debug

---
+++ cpp/ch4/gambling/player.hpp
@@ -0,0 +1,26 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+class player{
+ private:
+  string name;
+  int slots[3]; // array for random num
+ protected:
+  
+ public:
+  player();
+  player(string name);
+  ~player();
+
+
+  void setSlots();
+  string getName();
+  int* getSlots();
+};
+#endif
