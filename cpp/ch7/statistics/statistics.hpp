3cef0f3419af87114a04a149e74541c6c8bda83d
omegafrog <jiwoo22110103@gmail.com>
2020-12-30 10:33:32 +0000

    statistics class clear

--- cpp/ch7/statistics/statistics.hpp
+++ cpp/ch7/statistics/statistics.hpp
@@ -1,7 +1,27 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
 
+#include <iostream>
+using namespace std;
 
-ㅎ
+#define MAX_BUFFER_SIZE 10
+
+class Statistics{
+private:
+  int* buffer; // array pointer
+  int size; // number of values.
+public:
+  Statistics();
+
+  
+  void operator>>(int& avg); // calculate average and set to avg.
+  Statistics& operator<<(int newValue); // append new value in to buffer.
+  bool operator!(); // values is empty, return 1. isEmpty()
+  void operator~(); // print statistic data
+
+  ~Statistics();
+};
+
+
 
 #endif /* STATISTICS_H */
