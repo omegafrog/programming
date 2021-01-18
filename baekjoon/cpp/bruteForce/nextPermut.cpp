3692428cbec96bd81df080ea147b3b060d42cbfc
omegafrog <jiwoo22110103@gmail.com>
2021-01-18 14:42:00 +0000

    nextputation clear

--- baekjoon/cpp/bruteForce/nextPermut.cpp
+++ baekjoon/cpp/bruteForce/nextPermut.cpp
@@ -1,19 +1,48 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-void findPermut(vector<int>& input, int checkSize, int* num){
-	if(num>toNum(input)) { cout num; return; }
-	
-	int checkIndex = input.size()-checkSize;
-	
-	// checkSize자리 순열 찾기
-	vector<int*> permutList;
-	int *tmp = new int [input.size()];
-	for(int i=0;i<facorial(checkSize());++i){
-		for(int i=checkIndex;i<input.size();++i){
-			
-		}
-	}
-	
+void nextPermutation(vector<int> &input, int n) {
+  for (int i = n - 1; i > 0; --i) {
+    int j = i - 1;
+
+    if (n == 2) {
+      if (input[1] < input[0]) {
+        cout << -1;
+        return;
+      }
+      int tmp = input[1];
+      input[1] = input[0];
+      input[0] = tmp;
+      cout << input[0] << " " << input[1];
+      return;
+    }
+
+    if (input[i] < input[j])
+      continue;
+
+    else {
+      next_permutation(input.begin() + 1, input.end());
+      for (int i = 0; i < n; i++) {
+        // cout << i << endl;
+        cout << input[i] << " ";
+      }
+
+      return;
+    }
+  }
+  cout << -1;
+}
+
+int main() {
+  int n = 0;
+  cin >> n;
+  vector<int> input;
+  int j = 0;
+  for (int i = 0; i < n; i++) {
+    cin >> j;
+    input.push_back(j);
+  }
+  nextPermutation(input, n);
 }
