178c49fa0ca1ed572f56a046e4f49f1e1bee8fbb
김지우 <jiwoo2211@naver.com>
2021-01-18 13:03:21 +0000

    baekjoon bruteForce

---
+++ baekjoon/cpp/bruteForce/nextPermut.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void findPermut(vector<int>& input, int checkSize, int* num){
+	if(num>toNum(input)) { cout num; return; }
+	
+	int checkIndex = input.size()-checkSize;
+	
+	// checkSize자리 순열 찾기
+	vector<int*> permutList;
+	int *tmp = new int [input.size()];
+	for(int i=0;i<facorial(checkSize());++i){
+		for(int i=checkIndex;i<input.size();++i){
+			
+		}
+	}
+	
+}
