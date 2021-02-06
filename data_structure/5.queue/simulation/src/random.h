b2aa2840d492cc8e4980cc5002f5a7082cc67a28
김지우 <jiwoo2211@naver.com>
2021-02-06 23:59:16 +0000

    unpushed commits

--- data_structure/5.queue/simulation/src/random.h
+++
@@ -1,6 +0,0 @@
-#ifndef __RANDOM_H__
-#define __RANDOM_H__
-
-int getrandom(int start, int end,int clock);
-
-#endif
--- data_structure/8.graph/2.graph/MST/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "mst.h"
-
-#endif
