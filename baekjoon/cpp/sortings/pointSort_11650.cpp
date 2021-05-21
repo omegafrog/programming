e59e4bfd6a125c8d64152146c5f31450b4e8f531
김지우 <jiwoo2211@naver.com>
2021-05-21 13:08:45 +0000

    baekjoon

---
+++ baekjoon/cpp/sortings/pointSort_11650.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+typedef struct point{
+    int x;
+    int y;
+} point;
+
+int N;
+
+bool compare(point A, point B){
+    return A.x<B.x;
+}
+
+int main(){
+    vector<point> A;
+    
+}
